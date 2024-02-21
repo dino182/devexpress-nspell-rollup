2456079a1f75a9e565954eed53dd1e0b6428c7db
Dino Infantino <dino.infantino@displayplan.com>
2024-02-21 17:38:30 +0000

    Initial repo

---
+++ rollup.config.js
@@ -0,0 +1,23 @@
+﻿import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import typescript from '@rollup/plugin-typescript';
+import { string } from "rollup-plugin-string";
+
+export default {
+    input: {
+        richedit: "index.ts"
+    },
+    output: {
+        dir: "dist",
+        entryFileNames: "[name].js",
+        format: "es"
+    },
+    plugins: [
+        resolve(),
+        typescript(),
+        string({
+            include: ["**/*.aff", "**/*.dic"]
+        }),
+        commonjs()
+    ]
+};
